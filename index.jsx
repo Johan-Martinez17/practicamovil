7a9e131e95fbffa1c070c1273d73ad2b41ca3521
Johan <johan_mtinez@gmail.com>
2024-08-20 10:19:18 -0500

    barra de navegacion

---
+++ index.jsx
@@ -0,0 +1,20 @@
+import {Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
+
+export default function App() {
+  return (
+    
+    <View className="flex-1 items-center justify-center bg-white">
+
+    
+      <Text className ="text-3xl font-pblack"> Johan Martinez </Text>
+      <StatusBar style="auto"/>
+      <Link href="/home" style={{color:'blue'}}>go to home</Link>
+
+    
+    </View>
+  
+)
+}
+
